9090edf3fe10a130e6a3388b77dd5bb5a57199a6
Kevin <kevinacontrerasl62@gmail.com>
2022-01-04 16:05:46 -0600

    cambio de texto2

--- back-end/router/router.js
+++ back-end/router/router.js
@@ -5,28 +5,5 @@ rutas.get("/",function(req,res,next){
   
   res.render("index.html")
 })
-rutas.post('/create-checkout-session', async (req, res) => {
 
-  const session = await stripe.checkout.sessions.create({
-
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'TAREA ESTADISTICA 1 USAC',
-
-          },
-          unit_amount: 1000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: 'http://localhost:3000/success.html',
-    cancel_url: 'http://localhost:3000/index.html',
-  });
-
-  res.redirect(303, session.url);
-});
 module.exports = rutas
